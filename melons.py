24edfb3a54d84fb5eac8bd0c819adb24544ecd3b
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-17 17:30:06 -0800

    Finished melons.py with class for Government Orders

--- melons.py
+++ melons.py
@@ -9,6 +9,7 @@ class AbstractMelonOrder():
         self.species = species
         self.qty = qty
         self.shipped = False
+        self.flat_fee = 0
         if country_code:
             self.country_code = country_code
 
@@ -16,7 +17,10 @@ class AbstractMelonOrder():
         """Calculate price, including tax."""
 
         base_price = 5
-        total = (1 + self.tax) * self.qty * base_price
+        if self.species == "Christmas":
+            base_price = 1.5*base_price
+
+        total = (1 + self.tax) * self.qty * base_price + self.flat_fee
 
         return total
     
@@ -38,13 +42,25 @@ class DomesticMelonOrder(AbstractMelonOrder):
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
     order_type = "international"
-    
-    def __init__(self, species, qty):
+
+    def __init__(self, species, qty, country_code):
         super().__init__(species, qty)
         self.tax = 0.17
-     
+        if self.qty < 10:
+            self.flat_fee = 3
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """ """
+
+    def __init__(self, species, qty):
+        super().__init__(species, qty)
+        self.passed_inspection = False
+        self.tax = 0.00
+
+    def mark_inspection(self, status=False):
+        self.passed_inspection = status
